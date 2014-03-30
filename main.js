eaec24f651c596ee8884aaa195266e97b0b5645a
dtredger <d.tredger@gmail.com>
2014-03-30 16:00:05 -0400

    bird moves on spacebar, falls with gravity

--- main.js
+++ main.js
@@ -4,17 +4,37 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 // Creates a new 'main' state that wil contain the game
 var main_state = {
 
-    preload: function() { 
-		// Function called first to load all the assets
+	// Function called first to load all the assets
+    preload: function() {
+    	this.game.state.backgroundColor = '#71c5cf';
+    	this.game.load.image('bird', 'assets/bird.png');
     },
 
+    // Fuction called after 'preload' to setup the game
     create: function() { 
-    	// Fuction called after 'preload' to setup the game    
+		this.bird = this.game.add.sprite(100, 245, 'bird');
+		this.bird.body.gravity.y = 1000;
+
+		var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		space_key.onDown.add(this.jump, this);   	    
     },
     
+    // Function called 60 times per second
     update: function() {
-		// Function called 60 times per second
+		if (this.bird.inWorld == false) {
+			this.restart_game();
+		}
     },
+
+
+    jump: function() {
+    	this.bird.body.velocity.y = -200;
+    },
+
+    restart_game: function() {
+    	this.game.state.start('main');
+    }
+    
 };
 
 // Add and start the 'main' state to start the game
