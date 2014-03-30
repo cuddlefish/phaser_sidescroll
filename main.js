febf82085b2023da8db764595162f0d717672484
dtredger <d.tredger@gmail.com>
2014-03-30 16:29:40 -0400

    added pipes every 1.5sec

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ var main_state = {
     preload: function() {
     	this.game.state.backgroundColor = '#71c5cf';
     	this.game.load.image('bird', 'assets/bird.png');
+    	this.game.load.image('pipe', 'assets/pipe.png');
     },
 
     // Fuction called after 'preload' to setup the game
@@ -16,7 +17,12 @@ var main_state = {
 		this.bird.body.gravity.y = 1000;
 
 		var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-		space_key.onDown.add(this.jump, this);   	    
+		space_key.onDown.add(this.jump, this);
+
+		this.pipes = game.add.group();
+		this.pipes.createMultiple(20, 'pipe');
+
+		this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);	    
     },
     
     // Function called 60 times per second
@@ -26,13 +32,32 @@ var main_state = {
 		}
     },
 
+    // ---- custom functions ---- //
 
     jump: function() {
     	this.bird.body.velocity.y = -200;
     },
 
     restart_game: function() {
+    	this.game.time.events.remove(this.timer);
     	this.game.state.start('main');
+    },
+
+    add_one_pipe: function(x, y) {
+    	var pipe = this.pipes.getFirstDead();
+    	pipe.reset(x, y);
+
+    	pipe.body.velocity.x = -200;
+    	pipe.outOfBoundsKill = true;
+    },
+
+    add_row_of_pipes: function() {
+    	var hole = Math.floor(Math.random()*5) + 1;
+    	for (var i = 0; i < 8; i++) {
+    		if (i != hole && i != hole +1) {
+    			this.add_one_pipe(400, i*60 + 10);
+    		}
+    	}
     }
     
 };
